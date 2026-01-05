be63edf54f12ba4ff559cf25624d1c94c297766e
Zaheer Ali <zaheercode09@gmail.com>
2026-01-05 17:36:33 +0500

    stddata

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react' 
+import './App.css'
+import Stdform from '../src/Components/Task-one/Stdform'
+function App() {
+
+  return (
+    <>
+     {/* <h1 className='text-red-600'>Hello</h1> */}
+     <Stdform />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Task-one/Stdform.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+
+function App() {
+
+    let data = [
+        {
+            id: 1,
+            name: "Zaheer Ali",
+            RoollNo: 390102,
+            Batch: "2024",
+            FavoriteLanguage: "Javascript"
+        },
+        {
+            id: 2,
+            name: "Mubeen",
+            RoollNo: 127027,
+            Batch: "2024",
+            FavoriteLanguage: "Nodejs"
+        }, {
+            id: 3,
+            name: "Ammad Bhai",
+            RoollNo: 324234,
+            Batch: "2024",
+            FavoriteLanguage: "HTML"
+        }
+    ]
+    return (
+        <>
+            <div className="w-full w-[100vh] flex justify-center items-center gap-20">
+                <form action="" className="w-[300px] border-1 h-[300px] rounded-xl py-10 flex flex-col gap-8 text-center">
+                    <h1>SMIT STUDENT</h1>
+                    <p>Saylani Mass IT Training Program</p>
+                    <h4>Student Name : Zaheer Ali</h4>
+                    <h4>Student RollNo : 390102</h4>
+                    <h4>Batch : 16</h4>
+                </form>
+                <form action="" className="w-[300px] border-1 h-[300px] rounded-xl py-10 flex flex-col gap-8 text-center">
+                    <h1>SMIT STUDENT</h1>
+                    <p>Saylani Mass IT Training Program</p>
+                    <h4>Student Name : Zaheer Ali</h4>
+                    <h4>Student RollNo : 390102</h4>
+                    <h4>Batch : 16</h4>
+                </form><form action="" className="w-[300px] border-1 h-[300px] rounded-xl py-10 flex flex-col gap-8 text-center">
+                    <h1>SMIT STUDENT</h1>
+                    <p>Saylani Mass IT Training Program</p>
+                    <h4>Student Name : Zaheer Ali</h4>
+                    <h4>Student RollNo : 390102</h4>
+                    <h4>Batch : 16</h4>
+                </form>
+
+            </div>
+        </>
+    )
+}
+
+export default App
